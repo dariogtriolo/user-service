2ddcf7235a1aa68108437133681ddcc0e83f8081
dariogiovannitrolo <dariogiovannitrolo@gmail.com>
2022-10-14 19:19:57 +0200

    First commit, set up project

---
+++ src/test/java/it/triolod/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.triolod.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
